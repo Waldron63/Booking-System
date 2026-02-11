171a51ed7fb39d280c7448145bb01f0d92038544
Waldron63 <ingsis.gualdron.santiago@gmail.com>
2026-02-11 18:27:45 -0500

    configuracion de JwtRequestFilter

---
+++ src/main/java/edu/co/bookingSystem/security/JwtUserDetailService.java
@@ -0,0 +1,31 @@
+package edu.co.bookingSystem.security;
+
+import edu.co.bookingSystem.model.User;
+import edu.co.bookingSystem.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class JwtUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public JwtUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
